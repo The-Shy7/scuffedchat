927c5378df718f55824f1805e71b784f6b2b727d
tahmit <tahmit@uw.edu>
2020-01-17 09:05:00 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <main>
+    <header>
+      <img src="https://banner2.cleanpng.com/20180423/gfe/kisspng-livechat-online-chat-logo-computer-icons-live-chat-5add970945d006.339032601524471561286.jpg"/>
+      
+      <p>Scuffed Chat</p>
+    </header>
+
+    <div class="chat">
+      <div class="mine messages">
+        <div class="message last">
+          Dude
+        </div>
+      </div>
+
+      <div class="yours messages">
+        <div class="message">
+          Hey!
+        </div>
+
+        <div class="message">
+          You there?
+        </div>
+
+        <div class="message last">
+          Hello, how's it going?
+        </div>
+      </div>
+
+      <div class="mine messages">
+        <div class="message">
+          Great thanks!
+        </div> 
+        
+        <div class="message last">
+          How about you?
+        </div>
+      </div>
     </div>
-  );
+    
+    <TextInput onSend={t=> console.log(t)}/>
+  </main>
+}
+
+function TextInput(props){
+  const [text, setText] = useState()
+
+  return <div className="text-in">
+    <form>
+      <input value={text}
+      placeholder = "Write something" 
+      onChange={e => setText(e.target.value)}
+      />
+
+      <button onClick={()=> {
+        props.onSend(text)
+        setText('')
+      }}>
+      &uarr;
+      </button>
+    </form>
+  </div>
 }
 
 export default App;
